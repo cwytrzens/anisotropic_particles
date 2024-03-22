6f7c8b0d23cc4e387cf7a4931838e036fc9751ed
SteffenPL <steffen.plunder@web.de>
2024-03-22 14:20:08 +0100

    fix typo

--- simulate.jl
+++ simulate.jl
@@ -2,9 +2,9 @@ include("definitions.jl")
 
 
 p = loadparameters("params.toml")
-p = @set p.N = 50
-p = @set p.Lx = 20.0
-p = @set p.Ly = 20.0
+p = @set p.N = 300
+p = @set p.Lx = 40.0
+p = @set p.Ly = 40.0
 
 Random.seed!(0)  # set random seed
 s = init(p)
@@ -13,7 +13,7 @@ ts, sol = simulate(s, p)
 #result_1 = deepcopy( (;ts, sol) )
 #result_2 = deepcopy( (;ts, sol) )
 
-sum( norm(x - y) for (x,y) in zip(sol[end-1].X, result_2.sol[end-1].X))
+# sum( norm(x - y) for (x,y) in zip(sol[end-1].X, result_2.sol[end-1].X))
 
 ####
 # Single plot
@@ -36,20 +36,20 @@ fig = Figure()
 sl = Slider(fig[2,1], range = eachindex(sol))
 s_obs = @lift sol[$(sl.value)]
 
-init_plot(s_obs, p, fig)
+init_plot(s_obs, p, fig[1,1])
 fig
 
 
 #####
 # Interactive Window, compare
 #####
-fig = Figure()
-sl = Slider(fig[2,1], range = eachindex(sol))
-s_obs = @lift sol[$(sl.value)]
+# fig = Figure()
+# sl = Slider(fig[2,1], range = eachindex(sol))
+# s_obs = @lift sol[$(sl.value)]
 
-init_plot(s_obs, p, fig[1,1])
+# init_plot(s_obs, p, fig[1,1])
 
-s_obs_2 = @lift result_1.sol[$(sl.value)]
+# s_obs_2 = @lift result_1.sol[$(sl.value)]
 
-init_plot(s_obs_2, p, fig[1,2])
-fig
+# init_plot(s_obs_2, p, fig[1,2])
+# fig
