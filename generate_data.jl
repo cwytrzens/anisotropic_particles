e1e806ec02ce9c39ec2a4876a77944119766f20f
cwytrzens <a00825785@unet.univie.ac.at>
2024-06-03 11:05:07 +0200

    generate script

--- generate_data.jl
+++ generate_data.jl
@@ -2,85 +2,107 @@ include("definitions.jl")
 
 
 
-input_folder="input"
-output_folder="output"
+function generate_data(p, output_base, sim_name)
+    Random.seed!(0)  # set random seed
+    s = init(p)
+    ts, sol = simulate(s, p)
+
+    at(ts, sol, t) = sol[findfirst(ts .>= t)]
+
+    #result_1 = deepcopy( (;ts, sol) )
+    #result_2 = deepcopy( (;ts, sol) )
+
+    # sum( norm(x - y) for (x,y) in zip(sol[end-1].X, result_2.sol[end-1].X))
+
+    ####
+    # Single plot
+    ####
+    begin
+        Makie.inline!(true)
+        fig = Figure(size=(1800, 600))
+
+        ax = Axis(fig[1, 1], aspect=DataAspect(), title="init")
+
+        s = at(ts, sol, p.t_start)
+        X = Point2f.(s.X)
+        U = Point2f.(sincos.(s.theta))
+        E = [ellipse(s.X[i], s.theta[i], p) for i in 1:p.N]
+        poly!(ax, E)
+
+
+
+        ax = Axis(fig[1, 2], aspect=DataAspect(), title="middle")
+
+        s = at(ts, sol, p.t_end / 2)
+        X = Point2f.(s.X)
+        U = Point2f.(sincos.(s.theta))
+        E = [ellipse(s.X[i], s.theta[i], p) for i in 1:p.N]
+        poly!(ax, E)
+
+        ax = Axis(fig[1, 3], aspect=DataAspect(), title="terminal")
+
+        s = at(ts, sol, p.t_end)
+        X = Point2f.(s.X)
+        U = Point2f.(sincos.(s.theta))
+        E = [ellipse(s.X[i], s.theta[i], p) for i in 1:p.N]
+        poly!(ax, E)
+
+        Label(fig[0,1:3], sim_name, fontsize = 32)
+
+        save(joinpath(output_base, "snapshots.png"), fig)
+        fig
+    end
+
+
+
+    ####
+    # Make video 
+    ####
+
+    Makie.inline!(false)
+    s_obs = Observable(s)
+    fig = init_plot(s_obs, p)
+    ax = content(fig[1,1])
+    record(fig, joinpath(output_base, "movie.mp4"), eachindex(sol)) do i 
+        s_obs[] = sol[i]  # update state
+        ax.title = sim_name * " t = $(round(ts[i], digits = 2))" 
+    end
+
+end
+
+
+
+
+input_folder = "input"
+output_folder = "output"
 
 
 mkpath(output_folder)
-redo=true
+redo = true
 
-for (root,folder,files) in walkdir(input_folder)
-    
+for (root, folder, files) in walkdir(input_folder)
+
     for fn in files
 
-        param_fn=joinpath(root,fn)
-        sim_name=splitext(fn)[1]
-        output_base=joinpath(output_folder,relpath(root,input_folder),sim_name)
+        param_fn = joinpath(root, fn)
+        sim_name = splitext(fn)[1]
+        output_base = joinpath(output_folder, relpath(root, input_folder), sim_name)
         mkpath(output_base)
 
         if !isdir(output_base) || redo
-            cp(param_fn,joinpath(output_base,"param.toml"),force=true)
+            cp(param_fn, joinpath(output_base, "param.toml"), force=true)
 
             p = loadparameters(param_fn)
-            printstyled("generate $output_base, $sim_name\n",color=:green)
-            #generate_data(p,output_base,sim_name)
+            printstyled("generate $output_base, $sim_name\n", color=:green)
+            generate_data(p,output_base,sim_name)
         end
-       
+
     end
 end
 
 
 
-p=loadparameters("params.toml")
-output_base="output\\Du\\params"
-sim_name="test"
+# p = loadparameters("params.toml")
+# output_base = "output\\Du\\params"
+# sim_name = "test"
 
-
-
-Random.seed!(0)  # set random seed
-s = init(p)
-ts, sol = simulate(s, p)
-
-#result_1 = deepcopy( (;ts, sol) )
-#result_2 = deepcopy( (;ts, sol) )
-
-# sum( norm(x - y) for (x,y) in zip(sol[end-1].X, result_2.sol[end-1].X))
-
-####
-# Single plot
-####
-# s_obs = Observable(s)
-# fig = init_plot(s_obs, p)
-
-####
-# Make video 
-####
-# record(fig, "movie.mkv", eachindex(sol)) do i 
-#     s_obs[] = sol[i]  # update state
-# end
-
-
-#####
-# Interactive Window 
-#####
-fig = Figure()
-sl = Slider(fig[2,1], range = eachindex(sol))
-s_obs = @lift sol[$(sl.value)]
-
-init_plot(s_obs, p, fig[1,1])
-fig
-
-
-#####
-# Interactive Window, compare
-#####
-# fig = Figure()
-# sl = Slider(fig[2,1], range = eachindex(sol))
-# s_obs = @lift sol[$(sl.value)]
-
-# init_plot(s_obs, p, fig[1,1])
-
-# s_obs_2 = @lift result_1.sol[$(sl.value)]
-
-# init_plot(s_obs_2, p, fig[1,2])
-# fig
